64d7439962f9c74f21b6cc94f5e85249765994b0
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-04-21 11:07:42 +0800

    UPDATED: project structure

--- mirror/src/androidTest/java/com/scmp/mirror/ExampleInstrumentedTest.kt
+++ mirror/src/androidTest/java/com/scmp/mirror/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.scmp.mirror", appContext.packageName)
+        assertEquals("com.scmp.mirror.test", appContext.packageName)
     }
 }
