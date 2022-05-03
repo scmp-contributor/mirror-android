935ee361c9884b9aa35fc960102b07c671d68bdc
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-05-03 17:28:22 +0800

    UPDATED: the sample code of mirror

--- mirror-sample/src/main/java/com/scmp/mirror/MainActivity.kt
+++ mirror-sample/src/main/java/com/scmp/mirror/MainActivity.kt
@@ -9,15 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mirrorAPI = MirrorAPI(context = this, domain = "www.scmp.com", isDebug = true)
+        /** init the mirror api with below one line code */
+        MirrorAPI(context = this, domain = "www.scmp.com", isDebug = true)
+
         val pingButton = findViewById<Button>(R.id.main_activity_ping_button)
         pingButton.setOnClickListener {
-            mirrorAPI.ping(TrackData(path = "testing.com"))
+            MirrorAPI.instance.ping(TrackData(path = "testing.com"))
         }
 
         val clickButton = findViewById<Button>(R.id.main_activity_click_button)
         clickButton.setOnClickListener {
-            mirrorAPI.click(TrackData(path = "testing.com"))
+            MirrorAPI.instance.click(TrackData(path = "testing.com"))
         }
     }
 }
